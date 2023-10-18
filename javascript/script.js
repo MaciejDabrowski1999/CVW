7ec0353ae5dc776aba752c5318f98c1de605ca0c
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-10-18 10:29:12 +0200

    add php from

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,108 @@
-const formMessage = document.querySelector('.form form')
 const menuButton = document.querySelector('#menu-toggle')
 const menuList = document.querySelector('.menu')
-const inputName = document.querySelector('#name')
-const inputPhone = document.querySelector('#telephone')
-const inputEmail = document.querySelector('#email')
+const name = document.querySelector('#name')
+const phone = document.querySelector('#phone')
+const email = document.querySelector('#email')
 const message = document.querySelector('#message')
-const submit = document.querySelector('.send')
-const divErrorName = document.querySelector('.error-form-name')
-const divErrorPhone = document.querySelector('.error-form-phone')
-const divErrorEmail = document.querySelector('.error-form-mail')
+const submit = document.querySelector('.submit')
+const msgStatus = document.querySelector('.msg-status')
+
+console.log(document.location.search)
+
+const mailFunction = () => {
+	if (document.location.search === '?mail_status=sent') {
+		msgStatus.classList.add('success')
+		msgStatus.textContent = 'Wiadomość wysłana!'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('success')
+			msgStatus.textContent = ''
+		}, 3000)
+	} else if (document.location.search === '?mail_status=error') {
+		msgStatus.classList.add('error-php')
+		msgStatus.textContent = 'Wystąpił błąd'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('error-php')
+			msgStatus.textContent = ''
+		}, 3000)
+	}
+}
 
 const closeMenu = () => {
 	menuButton.checked = false
 }
-function validateForm() {
-	const testEmail =
-		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-	const testPhone = /[0-9]{9}/
-	if (
-		inputName.value.trim() !== '' &&
-		inputEmail.value.trim() !== '' &&
-		testEmail.test(inputEmail.value) &&
-		inputPhone.value.trim() !== '' &&
-		inputPhone.value.trim() !== '' &&
-		testPhone.test(inputPhone.value) === true
-	) {
-		divErrorName.textContent = ''
-		divErrorEmail.textContent = ''
-		divErrorPhone.textContent = ''
-		console.log('wszystko ok')
-		return false
-	} else if (inputName.value === '' && inputEmail.value.trim() === '' && inputPhone.value.trim() === '') {
-		divErrorName.textContent = 'Proszę wypełnić pola formularza.'
-		divErrorEmail.textContent = 'Proszę wypełnić pola formularza.'
-		divErrorPhone.textContent = 'Proszę wypełnić pola formularza.'
+
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
+
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+const checkLength = (input, min) => {
+	if (input.value.length < min && input === name) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} jest za krótkie`)
+	} else if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} jest za krótka`)
 	}
-	if (inputName.value.trim() === '') {
-		divErrorName.textContent = 'Wpisz swoje Imię'
-	}
-	if (inputEmail.value.trim() === '' || testEmail.test(inputEmail.value) === false) {
-		divErrorEmail.textContent = 'Proszę wpisać poprawny email'
+}
+const checkEmail = email => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(email.value)) {
+		clearError(email)
 	} else {
-		divErrorEmail.textContent = ''
-	}
-	if (inputPhone.value.trim() === '') {
-		divErrorPhone.textContent = 'Proszę wpisać 9 cyfrowy numer'
-	} else if (inputPhone.value.trim() !== '' && testPhone.test(inputPhone.value) === false) {
-		divErrorPhone.textContent = 'Niepoprawny numer.'
-	} else {
-		divErrorPhone.textContent = ''
+		showError(email, 'E-mail jest niepoprawny przykład: example@example.com')
 	}
 }
 
-submit.addEventListener('click', validateForm)
-menuList.addEventListener('click', closeMenu)
-formMessage.addEventListener('click', e => {
+const checkPhone = phone => {
+	const re = /\b(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}\b/
+	if (re.test(phone.value)) {
+		clearError(phone)
+	} else {
+		showError(phone, 'Numer jest niepoprawny')
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		mailFunction()
+	}
+}
+submit.addEventListener('click', e => {
 	e.preventDefault()
+
+	checkForm([name, phone, email])
+	checkEmail(email)
+	checkLength(name, 3)
+	checkLength(message, 5)
+	checkPhone(phone)
+	checkErrors()
 })
+
+menuList.addEventListener('click', closeMenu)
---
+++ php/mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST["name"];
+$from = $_POST["mail"];
+$phone = $_POST["phone"];
+$message = $_POST["msg"];
+$subject = "Wiadomość z formularza dabrowskiwebsite.pl";
+$to = "maciej.dabrowski299@gmail.com";
+
+$txt = "Imię " . $name . "\r\n" . "Email: " . $from . "\r\n" . "Telefon: " . $phone . "\r\n" . "\r\n" . "Treść: " . $message;
+
+$headers = "From: " . $from . "\r\n";
+$headers = "Replay-To: " . $from . "\r\n";
+
+$mail_status = mail($to, $subject, $txt, $headers);
+
+if ($mail_status){
+    header("Location: /index.html?mail_status=sent");
+} else{
+    header("Location: /index.html?mail_status=error");
+}
